81e564312b464308b3ee0230a988fa692eb03da6
Kutman Shirinbekov <simpleshirinbekov@gmail.com>
2020-04-22 02:01:37 +0600

    add templates, and etc

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
 
-
+app_name = 'app'
 urlpatterns = [
+#ex: /app/
     path('', views.index, name='index'),
+#ex: /app/2/
+    path('<int:question_id>/', views.detail, name='detail'),
+#ex: /app/2/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+#ex: /app/2/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+
 ]
--- forum/settings.py
+++ forum/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'forum.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -108,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Bishkek'
 
 USE_I18N = True
 
